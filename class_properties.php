0a575532102ea7803baa6daf576ceb4d99edd331
akanmayowa <akanmayowa@yahoo.com>
2022-01-28 01:40:11 +0100

    second commit

--- class_properties.php
+++ class_properties.php
@@ -4,15 +4,33 @@ Class Student{
 var $first_name;
 var $last_name;
 var $country = 'None';
+
+
+public function say_hello(){
+  return 'hello_world';
+}
+
+
+public function full_name(){
+
+  return $this->first_name. " " . $$this->last_name;
+}
+
 }
 
 
 
-$student1 = new Student();
-print $student1->first_name = 'lucky';
-print $student1->last_name = 'johnex';
+$student1 = new Student;
+print $student1->first_name = 'lucky'. "<br />";
+print $student1->last_name = 'johnex'. "<br />";
+echo $student1->full_name() . "<br />";
 
-$student2 = new Student();
+echo $student1->say_hello();. "<br />";
+echo $student2->say_hello();. "<br />";
+
+
+
+$student2 = new Student;
 $student2->first_name = 'lucky';
 $student2 ->last_name = 'johnex';
 
